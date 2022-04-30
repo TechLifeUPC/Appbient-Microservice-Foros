5f2bdf228dd2532cff94650683368ad9aed7c67c
eduperaltas <61795886+eduperaltas@users.noreply.github.com>
2022-04-30 15:21:56 -0500

    Initial commit

---
+++ src/main/java/com/example/appbient_microservice_foro/api/resource/ComentarioForo/CreateComentarioForoResource.java
@@ -0,0 +1,10 @@
+package com.example.appbient_microservice_foro.api.resource.ComentarioForo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateComentarioForoResource {
+    private String descripcion;
+}
